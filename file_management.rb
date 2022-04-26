9624bf4f737fb1ccd35df0b3d71b3a28b4a561c4
FridaWallstrom <93259332+FridaWallstrom@users.noreply.github.com>
2022-04-26 09:58:20 +0200

    v 17

--- file_management.rb
+++ file_management.rb
@@ -1,6 +1,23 @@
 def grab_diary(file_name)
     file_content = File.read(file_name)
-    day_array = file_content.split("\n\n", -1)
+    day_array = file_content.split("\n\n")
     day_array.map!{|day| day.lstrip.chomp}
-    return day_array
+
+    result = []
+    day_array.each_with_index do |str, i|
+        arr = str.split("\n")
+        result[i] = [arr[0], arr[1..-1].join("\n")]
+    end
+    return result
+end
+
+def save_changes(day_array, file_name)
+   
+    content = ""
+    day_array.each do |day|
+        content += day[0] + "\n"
+        content += day[1] + "\n\n"
+    end 
+    
+    File.write(file_name, content)
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,79 @@
 require_relative('file_management.rb')
 
-def handle_day(day, action)
+#lägg till raise 
+day_array = grab_diary('dagbok.txt')
 
+def print_alternatives(day_array)
+    puts "\n\nVälj en dag: "
+    day_array.each do |day|
+        puts day[0]
+    end
+end 
+
+def sort_days(day_array)
+    day_array = day_array.dup
+    output_list = []
+    while day_array.length > 0
+        i = 0
+        smallest = 0
+        while i < day_array.length 
+            if day_array[smallest][0] > day_array[i][0]
+                smallest = i
+            end 
+            i += 1
+        end 
+        output_list << day_array[smallest]
+        day_array.delete_at(smallest)
+    end 
+    return output_list
+end 
+
+def find_day(day_array, day_choice)
+    start = 0 
+    finish = day_array.length
+    while true 
+        middle_i = (start + ((finish - start)/2.0)).floor
+        if day_array[middle_i][0] == day_choice
+            return middle_i 
+        elsif start == finish
+            if day_array[start][0] == day_choice
+                return middle_i
+            else 
+                return nil
+            end 
+        elsif day_array[middle_i][0] < day_choice
+            start = middle_i
+        elsif day_array[middle_i][0] > day_choice
+            finish = middle_i
+        end 
+    end 
+end 
+
+def handle_day(action, day_array)
+    if action == 0
+        print_alternatives(day_array)
+        day_choice = gets().chomp
+        puts day_array[find_day(day_array, day_choice)]
+    elsif action == 1
+        #ska kunna skrolla, skriva in var som helst - text editor, offset 
+    elsif action == 2
+        puts "datum?"
+        day = gets().chomp 
+        puts "innehåll?"
+        content = gets().chomp
+        entry = [day] + [content]
+        day_array << entry
+        day_array = sort_days(day_array)
+        save_changes(day_array, 'dagbok.txt')
+        puts "added!"
+        return day_array
+    elsif action == 3
+        print_alternatives(day_array)
+        day_choice = gets().chomp
+        day_array.delete_at(find_day(day_array, day_choice))
+        save_changes(day_array, 'dagbok.txt')
+        return day_array
+    end 
 end
 
 while true
@@ -17,11 +89,6 @@ while true
         break
     end
 
-    puts "\n\nVälj en dag: "
-    grab_diary("dagbok.txt").each do |day|
-        puts day.split("\n", 2)[0] # Delar bara till två delar för att optimera, sedan skriver vi bara ut datumet
-    end
-    day_choice = gets().chomp
-
-    handle_day(day_choice, action)
+    handle_day(action, day_array)
 end
+
