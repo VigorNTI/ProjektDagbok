5c943e53bd097a3ed6b2274598d2e658e8acd066
FridaWallstrom <93259332+FridaWallstrom@users.noreply.github.com>
2022-05-03 10:07:14 +0200

    v 18

--- file_management.rb
+++ file_management.rb
@@ -1,10 +1,10 @@
 def grab_diary(file_name)
     file_content = File.read(file_name)
     day_array = file_content.split("\n\n")
-    day_array.map!{|day| day.lstrip.chomp}
+    day_array.map!{|day| day.lstrip.chomp} #delar upp för varje dag
 
     result = []
-    day_array.each_with_index do |str, i|
+    day_array.each_with_index do |str, i|#delar upp dag och innehåll 
         arr = str.split("\n")
         result[i] = [arr[0], arr[1..-1].join("\n")]
     end
@@ -12,7 +12,6 @@ def grab_diary(file_name)
 end
 
 def save_changes(day_array, file_name)
-   
     content = ""
     day_array.each do |day|
         content += day[0] + "\n"
@@ -21,3 +20,11 @@ def save_changes(day_array, file_name)
     
     File.write(file_name, content)
 end
+
+def grab_password(file_name)
+    file_content = File.read(file_name)
+end 
+
+def save_password(file_name)
+    File.write(file_name, content)
+end 
--- main.rb
+++ main.rb
@@ -3,14 +3,18 @@ require 'date'
 
 system("clear") || system("cls") 
 
+#variablar som anropar funktionerna i filhanteraren 
 day_array = grab_diary('dagbok.txt')
+password = grab_password('password.txt')
 
+#skriver ut alla alternativ för datum 
 def print_alternatives(day_array)
     day_array.each do |day|
         puts day[0]
     end
 end 
 
+#selection sort
 def sort_days(day_array)
     day_array = day_array.dup
     output_list = []
@@ -29,20 +33,19 @@ def sort_days(day_array)
     return output_list
 end 
 
+#binary search 
 def find_day(arr, target)
-    left    = 0
-    right   = arr.length - 1
+    left = 0
+    right = arr.length - 1
 
     while left <= right do
-        # start by finding the middle element between left and right
+
         middle = ( (right - left) / 2 ) + left
 
-        # check if we find target
         if arr[middle][0] == target
             return middle
         end
 
-        # change working area
         if arr[middle][0] < target
             left = middle + 1
         else
@@ -52,7 +55,7 @@ def find_day(arr, target)
     return nil
 end
 
-
+#kollar om varje dag är unik genom att jämföra alla datum med tidigare 
 def day_unique(day_array, day_choice)
     day_array.each do |unique|
         if unique[0].chomp == day_choice
@@ -62,6 +65,7 @@ def day_unique(day_array, day_choice)
     return true 
 end
 
+#hanterar alla alternativ och funktionen i dagboken, om inloggad
 def handle_day(action, day_array)
     if action == 0
         system("clear") || system("cls")
@@ -75,14 +79,13 @@ def handle_day(action, day_array)
             system("clear") || system("cls")
             puts "Valbara datum:"
             print_alternatives(day_array)
-            puts "" # blank line
+            puts ""
         end
         system("clear") || system("cls") 
         puts "Innehållet:"
         puts day_array[find_day(day_array, day)]
         puts ""
     elsif action == 1
-        #ska kunna skrolla, skriva in var som helst - text editor, offset 
         system("clear") || system("cls")
         day = ""
         loop do
@@ -94,7 +97,7 @@ def handle_day(action, day_array)
             system("clear") || system("cls")
             puts "Valbara datum:"
             print_alternatives(day_array)
-            puts "" # blank line
+            puts ""
         end
 
         # Validation and parsing
@@ -109,7 +112,7 @@ def handle_day(action, day_array)
         # Create file with contents from day #{day}
         day_idx = find_day(day_array, day)
         if day_idx == nil
-            system("clear") || system("cls")
+            system("clear") || system("cls")   
             puts "Datum '#{day}' finns ej, vänligen försök igen"
             puts '' # blank space
             return
@@ -121,14 +124,14 @@ def handle_day(action, day_array)
         puts "Din textredigerare kommer att öppna vald dag, spara och stäng dokumentet för att fortsätta"
 
         # Start the text editor and promt to continue
-        if !system("cmd /c 'start #{day}'", :err => File::NULL, :out => File::NULL) # Windows
+        if !system("start #{day}", :err => File::NULL, :out => File::NULL) # Windows
             if !system("xdg-open '#{day}'", :err => File::NULL, :out => File::NULL) # Linux
                 system("open '#{day}'", :err => File::NULL, :out => File::NULL) # Mac OSX, för er skull sir Widebrant ;)
             end
         end
         puts '' # blank line
         loop do
-            puts "Färdig? [J/n]"
+            puts "Färdig? [j/n]"
             if gets().chomp.downcase == 'j'
                 break
             end
@@ -140,12 +143,13 @@ def handle_day(action, day_array)
         save_changes(day_array, "dagbok.txt")
         system("clear") || system("cls")
         puts "Filen är nu sparad!"
-        puts '' # blank space
+        puts '' 
         File.delete(day) if File.exist?(day) # also remove the temporary file
     elsif action == 2
+        system("clear") || system("cls")
         puts "datum?"
         day = gets().chomp 
-        begin  # "try" block
+        begin 
             date = Date.strptime(day, '%Y-%m-%d')
             day = date.strftime('%Y-%m-%d')
             if day_unique(day_array, day)
@@ -166,34 +170,117 @@ def handle_day(action, day_array)
         end 
         return day_array
     elsif action == 3 
-        print_alternatives(day_array)
-        day_choice = gets().chomp
-        system("clear") || system("cls") 
-        if find_day(day_array, day_choice) == nil 
-            puts "datumet finns ej"
-        else 
-            day_array.delete_at(find_day(day_array, day_choice))
-            save_changes(day_array, 'dagbok.txt')
-            puts "borttaget!"
+        system("clear") || system("cls")
+        day_choice = ""
+        loop do
+            puts "Vilket datum vill du ta bort? Skriv 'visa' för att visa alla sparade datum."
+            day_choice = gets().chomp
+            if day_choice.downcase != "visa" then
+                break;
+            end
+            system("clear") || system("cls")
+            puts "Valbara datum:"
+            print_alternatives(day_array)
             puts ""
-            return day_array
-        end 
+        end
+
+        day_array.delete_at(find_day(day_array, day_choice))
+        save_changes(day_array, 'dagbok.txt')
+        puts "borttaget!"
+        puts ""
+        return day_array
     end 
 end
 
-while true
-    puts "Vad vill du göra med din dagbok?"
-    puts "0 - Öppna"
-    puts "1 - Redigera"
-    puts "2 - Lägg till"
-    puts "3 - Ta bort"
-    puts "4 - Avsluta"
-    action = gets().chomp.to_i
-
-    if action == 4 then
-        break
+#krypterar dagboken
+def encrypt_text(text)
+    encrypted_text = []
+    text.chars.each do |c|
+        c = c.downcase
+        if c.ord == 122 #z
+            encrypted_text << (65).chr 
+        elsif c.ord == 32 #mellanslag
+            encrypted_text << (32).chr
+        else
+            encrypted_text << (c.ord + 1).chr 
+        end 
     end
+    encrypted_text = encrypted_text.join
+    return encrypted_text
+end 
 
-    handle_day(action, day_array)
-end
+#hanterar den krypterade dagboken  
+def crypted_handle_day(action, day_array)
+    if action == 0
+        day = ""
+        loop do
+            puts "Vilket datum vill du öppna? Skriv 'visa' för att visa alla sparade datum."
+            day = gets().chomp
+            if day.downcase != "visa" then
+                break;
+            end
+            system("clear") || system("cls") 
+            puts "Valbara datum:"
+            print_alternatives(day_array)
+            puts "" 
+        end
 
+        system("clear") || system("cls") 
+        puts "Innehållet:"
+        puts encrypt_text(day_array[find_day(day_array, day)][1])
+        puts ""
+    else 
+        system("clear") || system("cls") 
+        puts "Alternativet är inte tillgängligt, logga in!"
+        puts ""
+    end 
+end 
+
+#huvuddelen - loop som kör hela programmet 
+puts "Logga in? [j/n]"
+login_answer = gets().chomp.downcase
+if login_answer == "j"
+    login = 0 
+    while login == 0
+        puts "Lösenord:"
+        entry = gets().chomp
+        if entry == password
+            login = 1
+        else 
+            puts "Fel lösenord"
+            puts "Lösenord:"
+            entry = gets().chomp
+        end 
+    end 
+    while true 
+        puts "Vad vill du göra med din dagbok?"
+        puts "0 - Öppna"
+        puts "1 - Redigera"
+        puts "2 - Lägg till"
+        puts "3 - Ta bort"
+        puts "4 - Avsluta"
+        action = gets().chomp.to_i
+
+        if action == 4 then
+            break
+        end
+
+        handle_day(action, day_array)
+    end
+else 
+    while true
+        puts "Vad vill du göra med din dagbok?"
+        puts "0 - Öppna"
+        puts "1 - Redigera"
+        puts "2 - Lägg till"
+        puts "3 - Ta bort"
+        puts "4 - Avsluta"
+        action = gets().chomp.to_i
+
+        if action == 4 then
+            break
+        end
+        
+        crypted_handle_day(action, day_array)
+    end
+end 
